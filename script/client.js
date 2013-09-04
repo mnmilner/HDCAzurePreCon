2ff66cc880c62b5d374b218b1e0ffe9f248c324c
adam grocholski <adam@thinkfirstcodelater.com>
2013-09-04 10:02:00 -0500

    removed service info

--- script/client.js
+++ script/client.js
@@ -3,7 +3,7 @@
 function azClient (){
     var self = this;
 
-    self.client = new WindowsAzure.MobileServiceClient("https://hdc13.azure-mobile.net/","JYfNaXuTCOjTJKRAUwmoEwhMgPAjna42");
+    self.client = new WindowsAzure.MobileServiceClient("<YOUR AZURE MOBILE SERVICE URL>",",<YOUR AZURE MOBILE SERVICE KEY>");
 
     //get all the sessions
     self.getSessions = function (callback) {
